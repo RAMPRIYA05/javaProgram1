be4827b02a6f47cee68bfd54cc7bfe31c16088de
Ramp3565 <Ramp3565@CSL0709.global.chainsys.com>
2024-04-29 14:19:53 +0530

    arraylist-add,clear,clone,boolean,size

---
+++ JavaProgram/src/com/chainsys/collection/Collection.java
@@ -0,0 +1,70 @@
+package com.chainsys.collection;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Stack;
+
+import com.chainsys.demo2.Book;
+
+public class Collection {
+
+	public static void main(String[] args)
+	{
+		ArrayList array=new ArrayList();
+		for(int i=1;i<=6;i++)
+		{
+			array.add(i);
+			System.out.println(array);
+		}
+		array.add(3,7);
+		System.out.println(array);
+		array.add("priya");
+		System.out.println(array);
+		
+		
+		ArrayList bookList=new ArrayList();
+		for(int i=1;i<2;i++) {
+		bookList.add("Thriller");
+		bookList.add("Gone girl");
+		bookList.add("2012");
+		System.out.println(bookList);
+		}
+		
+		bookList.clear();
+		System.out.println(bookList);
+		  array.clone();
+		  System.out.println(array);
+		  
+		  ArrayList<Boolean> arr1=new ArrayList<Boolean>();
+		  arr1.isEmpty();
+		  System.out.println(arr1);
+		  boolean arr2=arr1.isEmpty();
+		  if(arr2==true)
+		  {
+			  System.out.println("arr2 is empty");
+		  }
+		  else
+		  {
+			  System.out.println("arr2 is not empty");
+		  }
+		  System.out.println(array);
+		  int size=array.size();
+		  System.out.println("size:"+size);
+		
+		  HashSet hash=new HashSet();
+		  hash.add(1);
+		  hash.add(2);
+		  System.out.println(hash);
+		  hash.add(10);
+		  System.out.println(hash);
+		  
+		  Stack <String>stack=new <String>Stack();
+		  stack.push("Ram");
+		  stack.push("priya");
+		  stack.push("Rampriya");
+		  System.out.println(stack);
+		  stack.pop();
+		  System.out.println(stack);
+		  
+	}
+}
